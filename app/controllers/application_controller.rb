ac1ee0315d7d7a7e6b1e1468322d93019f3453e6
Victor de Oliveira <vitor.oliveira.marinho@gmail.com>
2025-05-02 09:44:16 -0300

    Turbo Drive

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  # To see progress bar in dev
+  # before_action -> { sleep 3 }
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,7 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+// To disable Turbo Drive
+// import { Turbo } from "@hotwired/turbo-rails"
+// Turbo.session.drive = false
