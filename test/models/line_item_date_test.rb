18ff3b2f526b9b6a2a29cf5a8c79a8d55d540a88
Victor de Oliveira <vitor.oliveira.marinho@gmail.com>
2025-05-24 10:38:34 -0300

    Nested Turbo Frames

--- test/models/line_item_date_test.rb
+++ test/models/line_item_date_test.rb
@@ -8,4 +8,31 @@ class LineItemDateTest < ActiveSupport::TestCase
   test "#previous_date returns nil when the quote has no previous date" do
     assert_nil line_item_dates(:today).previous_date
   end
+
+  test "Updating a line item date" do
+    assert_selector "h1", text: "First quote"
+  
+    within id: dom_id(@line_item_date, :edit) do
+      click_on "Edit"
+    end
+  
+    assert_selector "h1", text: "First quote"
+  
+    fill_in "Date", with: Date.current + 1.day
+    click_on "Update date"
+  
+    assert_text I18n.l(Date.current + 1.day, format: :long)
+  end
+  
+  test "Destroying a line item date" do
+    assert_text I18n.l(Date.current, format: :long)
+  
+    accept_confirm do
+      within id: dom_id(@line_item_date, :edit) do
+        click_on "Delete"
+      end
+    end
+  
+    assert_no_text I18n.l(Date.current, format: :long)
+  end
 end
